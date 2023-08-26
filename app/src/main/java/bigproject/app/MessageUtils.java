aa97d32f2a5a45bbc83e2ef95d6c1d3f839ccd46
24secondss <78076663+24secondss@users.noreply.github.com>
2023-08-26 17:04:52 +0200

    WXF-6-feat(AgeCheck): Added AgeCheck for User

--- app/src/main/java/bigproject/app/MessageUtils.java
+++ app/src/main/java/bigproject/app/MessageUtils.java
@@ -3,8 +3,19 @@
  */
 package bigproject.app;
 
+import java.util.Scanner;
+
 class MessageUtils {
     public static String getMessage() {
-        return "Hello      World!";
+        int age = getAge();
+        return "Hello Peter: You are " + age + " years old";
+    }
+
+    public static Integer getAge() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your Age:");
+        int age = scanner.nextInt();
+        scanner.close();
+        return age;
     }
 }
